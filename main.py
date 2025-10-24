410e50d10b6e81e1b7b7fc94b0cd08499ee86508
minispiki <140969853+minispiki@users.noreply.github.com>
2025-10-24 10:02:20 +0100

    finish v1

--- main.py
+++ main.py
@@ -1,6 +1,40 @@
-def main():
-    print("Hello from funnies!")
+import ollama
 
+# config
+name = "USER"
+command_prefix = "/"
+msg_query_suffix = "> " # including a space is recommended
+# end of config
+
+def main():
+    print("--Temp CHAT: all msgs are forgotten.--")
+    print(ollama.list())
+    print("Select your model")
+    model = input()
+    while True:
+        message = input(name+":"+ " " + msg_query_suffix)
+        ans = send(model, message)
+        if ans == 1:
+            print("Stopping [!!!]")
+            quit()
+        else:
+            print(ans)
+
+def send(mod, msg):
+    if command(msg) == 1:
+        return 1
+    response: ollama.ChatResponse = ollama.chat(model=mod, messages=[
+        {
+            'role': 'user',
+            'content': msg
+
+        },
+    ])
+    return (response['message']['content'])
+
+def command(msg):
+    if msg == (command_prefix + "exit") or msg == (command_prefix + "quit"):
+        return 1
 
 if __name__ == "__main__":
     main()
